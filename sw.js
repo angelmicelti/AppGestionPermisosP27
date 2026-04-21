682606bfc47bfe805521e1f62d266d528d679d34
angelmicelti <angelmicelti@users.noreply.github.com>
2026-04-22 00:27:13 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Nombre de la caché - CAMBIA ESTE NÚMERO CADA VEZ QUE ACTUALICES
-const CACHE_NAME = 'gestor-permisos-v1.22';  // <-- Cambia el número de versión
-const APP_VERSION = '1.22';
+const CACHE_NAME = 'gestor-permisos-v1.23';  // <-- Cambia el número de versión
+const APP_VERSION = '1.23';
 
 // Archivos a cachear
 const ARCHIVOS_CACHE = [
